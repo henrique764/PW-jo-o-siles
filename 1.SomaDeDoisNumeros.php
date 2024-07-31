dd357b1b7db0a27e511ff916d393b33ffe58baf8
Henrique <143546010+henrique764@users.noreply.github.com>
2024-07-31 10:38:27 -0300

    Add files via upload

---
+++ 1.SomaDeDoisNumeros.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+function sum($n1, $n2) {
+ $r = $n1 + $n2;
+ return $r;
+}
+
+echo "5 + 10 = " , sum(5, 10), "\n";
+echo "20 + 10 = ", sum(20, 10), "\n";
+echo "4 + 4 = ", sum(4, 4);
+?> 
+
+</body>
+</html>
